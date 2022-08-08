c17e71e2aed2f3abdde9879508fe40136013a5b2
durveshramane26 <durveshramane6@gmail.com>
2022-08-08 16:32:36 +0530

    added useReducer Hooks

--- src/Components/UseReducer.js
+++ src/Components/UseReducer.js
@@ -1,19 +1,32 @@
 import React, { useReducer} from 'react'
 import "./style.css";
 
+const reducer = (state, action) => {
+    if(action.type === 'INCR') {
+        state = state + 1;
+    }
+    
+    if(state > 0 && action.type === 'DECR') {
+        state = state - 1;
+    }
+    return state;
+}
+
 const UseReducer = () => {
+
+    const [state, dispatch] = useReducer(reducer, 0);
   return (
     <>
         <div className='center_div'>
-            <p></p>
-            <div className='button2'>
+            <p>{state}</p>
+            <div className='button2' onClick={() => dispatch({ type: "INCR"})}>
                 <span></span>
                 <span></span>
                 <span></span>
                 <span></span>
                 INCR
             </div>
-            <div className='button2'>
+            <div className='button2' onClick={() => dispatch({ type: "DECR" })}>
                 <span></span>
                 <span></span>
                 <span></span>
